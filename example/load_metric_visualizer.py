74ea4ace00d85f1d146dcf0608251a1c5dfbbcb1
yangheng95 <yangheng@m.scnu.edu.cn>
2022-07-24 20:24:41 +0100

    update

--- example/load_metric_visualizer.py
+++ example/load_metric_visualizer.py
@@ -6,18 +6,21 @@
 # huggingface: https://huggingface.co/yangheng
 # google scholar: https://scholar.google.com/citations?user=NPq5a_0AAAAJ&hl=en
 # Copyright (C) 2021. All Rights Reserved.
-
+import os
 
 import findfile
 from metric_visualizer import MetricVisualizer
 
 mv = MetricVisualizer.load(findfile.find_cwd_file('.mv'))
+save_path = os.getcwd()
+
 mv.summary()
-mv.traj_plot_by_trial()
-mv.violin_plot_by_trial()
-mv.box_plot_by_trial()
-mv.avg_bar_plot_by_trial()
-mv.sum_bar_plot_by_trial()
-mv.scott_knott_plot()
-mv.rank_test_by_trail()
-mv.rank_test_by_metric()
+
+mv.traj_plot_by_trial(save_path=save_path)
+mv.violin_plot_by_trial(save_path=save_path)
+mv.box_plot_by_trial(save_path=save_path)
+mv.avg_bar_plot_by_trial(save_path=save_path)
+mv.sum_bar_plot_by_trial(save_path=save_path)
+mv.scott_knott_plot(save_path=save_path, legend_loc=0)
+# mv.rank_test_by_trail()
+# mv.rank_test_by_metric()
