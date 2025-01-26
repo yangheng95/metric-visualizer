c347e11517abd478e2b05f28f07d6e2c7766f221
yangheng95 <yangheng@m.scnu.edu.cn>
2025-01-26 00:00:43 +0000

    0.9.16

--- metric_visualizer/utils.py
+++ metric_visualizer/utils.py
@@ -37,7 +37,6 @@ class MetricList:
         self.avg = np.nanmean(self.data)
         self.std = np.nanstd(self.data)
         self.median = np.nanmedian(self.data)
-        self.mode = stats.mode(self.data)[0][0]
         self.max = np.nanmax(self.data)
         self.min = np.nanmin(self.data)
         self.var = np.nanvar(self.data)
